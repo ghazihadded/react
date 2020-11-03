935c4537ba30367d31a9e01f8f5c199f9e0a58b3
ghazi hadded <ghazihadded6@gmail.com>
2020-11-03 21:06:43 +0100

    bootstrap form

---
+++ form-bootstrap/src/App.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import './App.css';
+import Form from './Form';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
